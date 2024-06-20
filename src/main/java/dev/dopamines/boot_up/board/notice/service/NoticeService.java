41dd7e0e541caadc990e65d0c9b71444e1d5ab1a
SihyunSeo <tjtlgus4@naver.com>
2024-06-21 00:42:34 +0900

    [Feat] Notice 게시판 게시글 전체 조회, id 조회 기능 구현

--- src/main/java/dev/dopamines/boot_up/board/notice/service/NoticeService.java
+++ src/main/java/dev/dopamines/boot_up/board/notice/service/NoticeService.java
@@ -2,8 +2,11 @@ package dev.dopamines.boot_up.board.notice.service;
 
 import dev.dopamines.boot_up.board.notice.model.request.NoticeCreateReq;
 import dev.dopamines.boot_up.board.notice.model.response.NoticeCreateRes;
+import dev.dopamines.boot_up.board.notice.model.response.NoticeReadRes;
 import dev.dopamines.boot_up.board.notice.repository.NoticeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NoticeService {
@@ -24,5 +27,17 @@ public class NoticeService {
         int result = noticeRepository.update(dto);
 
         return result;
+    }
+
+    public List<NoticeReadRes> findAll() {
+        List<NoticeReadRes> noticeReadResList = noticeRepository.findAll();
+
+        return noticeReadResList;
+    }
+
+    public NoticeReadRes find(int idx) {
+        NoticeReadRes noticeReadRes = noticeRepository.findById(idx);
+
+        return noticeReadRes;
     }
 }
