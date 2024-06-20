f54e79120678393536a61c380d86a1e77948ef38
syy0O <86238720+syy0O@users.noreply.github.com>
2024-06-20 16:20:19 +0900

    [Feat] project 게시판 게시글 DB 조회기능 구현

--- src/main/java/dev/dopamines/boot_up/board/project/repository/ProjectBoardRepository.java
+++ src/main/java/dev/dopamines/boot_up/board/project/repository/ProjectBoardRepository.java
@@ -1,6 +1,8 @@
 package dev.dopamines.boot_up.board.project.repository;
 
 import dev.dopamines.boot_up.board.project.model.request.ProjectBoardReq;
+import dev.dopamines.boot_up.board.project.model.response.ProjectBoardRes;
+import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -21,5 +23,42 @@ public class ProjectBoardRepository {
                 dto.getTeamName(),
                 dto.getMember()
         );
+    }
+
+    public ProjectBoardRes findById(int idx) {
+        ProjectBoardRes project = jdbcTemplate.queryForObject("SELECT * FROM project where idx = ?",
+                (rs, rownum) -> {
+                    ProjectBoardRes res = new ProjectBoardRes(
+                            rs.getLong("idx"),
+                            rs.getString("title"),
+                            rs.getString("video"),
+                            rs.getString("contents"),
+                            rs.getInt("course_num"),
+                            rs.getString("team_name"),
+                            rs.getString("member")
+                    );
+                    return res;
+                },
+                idx);
+        return project;
+    }
+
+    public List<ProjectBoardRes> findAll() {
+        List<ProjectBoardRes> projects = jdbcTemplate.query(
+                "SELECT * FROM project",
+                (rs, rownum) -> {
+                    ProjectBoardRes res = new ProjectBoardRes(
+                            rs.getLong("idx"),
+                            rs.getString("title"),
+                            rs.getString("video"),
+                            rs.getString("contents"),
+                            rs.getInt("course_num"),
+                            rs.getString("team_name"),
+                            rs.getString("member")
+                    );
+                    return res;
+                }
+        );
+        return projects;
     }
 }
