af82dda50a7792a8de570c8617cb598aa3b6df12
syy0O <86238720+syy0O@users.noreply.github.com>
2024-06-20 15:37:31 +0900

    [Feat] project 게시판 repository 구현

--- src/main/java/dev/dopamines/boot_up/board/project/repository/ProjectBoardRepository.java
+++ src/main/java/dev/dopamines/boot_up/board/project/repository/ProjectBoardRepository.java
@@ -1,2 +1,25 @@
-package dev.dopamines.boot_up.board.project.repository;public class ProjectBoardRepository {
+package dev.dopamines.boot_up.board.project.repository;
+
+import dev.dopamines.boot_up.board.project.model.request.ProjectBoardReq;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProjectBoardRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public ProjectBoardRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int save(ProjectBoardReq dto) {
+        return jdbcTemplate.update("INSERT INTO project (title, video, contents, course_num, team_name, member) VALUES (?,?,?,?,?,?)",
+                dto.getTitle(),
+                dto.getVideo(),
+                dto.getContents(),
+                dto.getCourseNum(),
+                dto.getTeamName(),
+                dto.getMember()
+        );
+    }
 }
