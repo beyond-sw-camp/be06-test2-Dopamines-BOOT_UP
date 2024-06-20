40f3fce8dc6b95d3d4a8c2f0dd10f64f363f3ef9
syy0O <86238720+syy0O@users.noreply.github.com>
2024-06-20 14:04:11 +0900

    init project

---
+++ src/main/java/dev/dopamines/boot_up/BootUpApplication.java
@@ -0,0 +1,13 @@
+package dev.dopamines.boot_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootUpApplication.class, args);
+    }
+
+}
